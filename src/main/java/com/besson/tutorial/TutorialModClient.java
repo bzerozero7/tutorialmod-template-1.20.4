bcd7f6e4b9eb1ebfab82a54849702710f1513981
ASUS <bzerozero7@gmail.com>
2024-06-12 21:29:07 +0800

    Default Changelist

---
+++ src/main/java/com/besson/tutorial/TutorialModClient.java
@@ -0,0 +1,9 @@
+package com.besson.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){}
+
+}
---
+++ src/main/java/com/besson/tutorial/item/ModItemGroup.java
@@ -0,0 +1,27 @@
+package com.besson.tutorial.item;
+
+import com.besson.tutorial.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+    public static final ItemGroup TUTORIAL_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(TutorialMod.MOD_ID,"tutorial_group"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemGroup.tutorial.group"))
+                    .icon(()->new ItemStack(ModItems.ICE_ETHER)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.ICE_ETHER);
+                        entries.add(Items.BOOK);
+                        entries.add(Blocks.CLAY);
+                    }).build());
+    public static void registerModItemGroup(){
+
+    }
+}
